b722de85ba4f407568cb0c86143bde9a2a0ba020
Alex H <166335521+Lexlilil@users.noreply.github.com>
2024-09-26 08:39:54 +0200

    Add files via upload

--- drawhouse/main.c
+++ drawhouse/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-// Function to build the base of the house (walls and floor)
+
 void build_base(int house_height) {
     for (int i = 0; i < house_height; i++) {
         printf("*");
@@ -10,7 +10,7 @@ void build_base(int house_height) {
         printf("*-|-|-|\n");
     }
 
-    // Floor of the house
+
     for (int i = 0; i < house_height + 2 + 2; i++) {
         printf("*");
     }
@@ -23,24 +23,24 @@ int main(void) {
 
     printf("Here is the house:\n");
 
-    // Roof
+
     int roof_height = (roof_width + 1) / 2;
     for (int i = 0; i < roof_height; i++) {
-        // Print spaces for left padding
+
         for (int j = 0; j < roof_height - i - 1; j++) {
             printf(" ");
         }
-        // Print roof structure
+
         for (int j = 0; j < i * 2 + 1; j++) {
             printf("*");
         }
         printf("\n");
     }
 
-    // Build the base (walls and floor)
+
     build_base(house_height);
 
-    // Fence
+
     for (int i = 0; i < fence_length; i++) {
         printf("*");
         for (int j = 0; j < house_height + 2; j++) {
